8481c1cdc3cc2ca8eef61331a6bb48afa03e8a3e
epot <emmanuel.pot@gmail.com>
2014-06-19 22:12:01 +0200

    fix local dev

--- ShoppingList/settings.py
+++ ShoppingList/settings.py
@@ -91,8 +91,9 @@ USE_L10N = True
 USE_TZ = True
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
