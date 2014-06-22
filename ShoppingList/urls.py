b5f8d2143a1a2b6f44b6953c298d4259e16a5afa
epot <emmanuel.pot@gmail.com>
2014-06-22 15:09:43 +0200

    default app is shopping

--- ShoppingList/urls.py
+++ ShoppingList/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponseRedirect
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,5 +10,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', lambda r : HttpResponseRedirect('shopping/')),
     url(r'^shopping/', include('shopping.urls', namespace='shopping')),
 )
